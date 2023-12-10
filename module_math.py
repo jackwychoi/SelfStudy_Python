a0e18cd69ca970b90a72d5eff6b5a2f5748b4cce
WONYOUNG CHOI <83526329+jackwychoi@users.noreply.github.com>
2023-12-10 19:15:47 +0900

    commit on 2023-12-10

---
+++ module_math.py
@@ -0,0 +1,23 @@
+import math
+
+print(math.sin(1)) # Sine
+print(math.cos(1)) # Cosine
+print(math.tan(1)) # Tangent
+print(math.floor(2.5)) # Round Down
+print(math.ceil(2.5)) # Round Up
+
+import math as m
+
+print(m.sin(1)) # Sine
+print(m.cos(1)) # Cosine
+print(m.tan(1)) # Tangent
+print(m.floor(2.5)) # Round Down
+print(m.ceil(2.5)) # Round Up
+
+from math import sin, cos, tan, floor, ceil
+
+print(sin(1)) # Sine
+print(cos(1)) # Cosine
+print(tan(1)) # Tangent
+print(floor(2.5)) # Round Down
+print(ceil(2.5)) # Round Up
